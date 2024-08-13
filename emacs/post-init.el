ad19b4834d10499f834028d3aca0b1e11948e73e
Alexander Danylyschyn <alex@dyyy.dev>
2024-08-13 21:46:10 +0200

    update emacs config

--- emacs/post-init.el
+++ emacs/post-init.el
@@ -1,22 +1,12 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-;; Disable the splash screen (to enable it agin, replace the t with 0)
-(setq inhibit-splash-screen t)
+;;; post-init.el --- post-init file -*- no-byte-compile: t; lexical-binding: t; -*-
 
 ;; Wrap Lines by default
 (global-visual-line-mode 1)
 
-;; Disable scrollbars
-(scroll-bar-mode -1)
-
 ;; theme
 (load-theme 'modus-vivendi)             ; Dark theme
 
+;; Org Modern Styling
 ;; Add frame borders and window dividers
 (modify-all-frames-parameters
  '((right-divider-width . 40)
@@ -53,16 +43,32 @@
 ;; Ellipsis styling
 (setq org-ellipsis "…")
 
-(global-org-modern-mode)
-
 (setq ns-right-alternate-modifier nil)
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode) ; enable line numbers globally
 
 ;; Add global keybindings
 (global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "M-o") #'ace-window)
-(global-set-key (kbd "M-0") #'treemacs-select-window)
+
+;; Setup treemacs
+
+(use-package treemacs
+  :ensure t
+  :bind
+  (("M-0" . treemacs-select-window)))
+
+;; Setup ace-window
+(use-package ace-window
+  :ensure t
+  :bind
+  (("M-o" . ace-window)))
+
+;; Setup org-modern
+(use-package org-modern
+  :ensure t
+  :after org)
+(add-hook 'org-mode-hook #'org-modern-mode)
+(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
 ;; org mode config
 (setq org-todo-keywords
@@ -90,4 +96,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "IosevkaTerm Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "IosevkaTerm Nerd Font" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
