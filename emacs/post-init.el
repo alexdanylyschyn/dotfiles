925d62eb67ff3bc4535c9369c124fd5eb0dbd06c
Alexander Danylyschyn <alex@dyyy.dev>
2025-01-16 14:21:00 +0100

    add emacs terraform stuff and imporve yabai

--- emacs/post-init.el
+++ emacs/post-init.el
@@ -88,6 +88,22 @@
         '(("smarty" . "\\.mjml\\'")))
   )
 
+;; Setup terraform
+(use-package terraform-mode
+  ;; if using straight
+  ;; :straight t
+
+  ;; if using package.el
+  ;; :ensure t
+  :custom (terraform-indent-level 4)
+  :config
+  (defun my-terraform-mode-init ()
+    ;; if you want to use outline-minor-mode
+    ;; (outline-minor-mode 1)
+    )
+
+  (add-hook 'terraform-mode-hook 'my-terraform-mode-init))
+
 ;; Setup Treesitter Modes
 (use-package treesit-auto
   :custom
---
+++ raycast/open_org.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Org Files
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName emacs
+
+open -a /Applications/EmacsClient.app ~/org
