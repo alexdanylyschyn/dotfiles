96ae0a9344217b66ce040b20a0b08ca9c95664c4
Alexander Danylyschyn <alex@dyyy.dev>
2025-10-03 21:53:08 +0200

    launch orgmode from raycast in nvim and kitty

--- raycast/open_org.sh
+++ raycast/open_org.sh
@@ -6,6 +6,9 @@
 # @raycast.mode silent
 #
 # Optional parameters:
-# @raycast.packageName emacs
+# @raycast.packageName nvim
 
-emacsclient -c -n -a emacs "$HOME/org"
+# emacsclient -c -n -a emacs "$HOME/org"
+# kitten @ --to=unix:/tmp/kitty launch --type=os-window --title=Org nvim "$HOME/org"
+export LANG=en_US.UTF-8
+/Applications/kitty.app/Contents/MacOS/kitty --single-instance zsh -c "nvim $HOME/org" &
