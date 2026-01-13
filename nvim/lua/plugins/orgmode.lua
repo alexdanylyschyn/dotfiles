719a05f9ff0bc1aae65e92150128a25e15996932
Alexander Danylyschyn <alex@dyyy.dev>
2026-01-13 11:06:14 +0100

    update lazyvim, disable relative line numbers, improve orgmode

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -14,6 +14,12 @@ return {
         org_default_notes_file = "~/org/misc.org",
         org_todo_keywords = { "TODO", "NEXT", "IN-PROGRESS", "WAITING", "DONE" },
         org_startup_indented = true,
+        mappings = {
+          org = {
+            org_todo = "<leader>ot",
+            org_set_tags_command = "<leader>oT",
+          },
+        },
       })
       require("org-bullets").setup()
       require("telescope").load_extension("orgmode")
